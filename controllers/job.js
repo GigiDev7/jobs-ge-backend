d08177e9c4f3df7950938b29c198d799d0edc235
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2022-01-09 17:42:11 +0400

    added update and delete controllers for job route

--- controllers/job.js
+++ controllers/job.js
@@ -1,8 +1,12 @@
 const Job = require("../models/job");
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find().populate("createdBy");
-  res.status(200).json(jobs);
+  try {
+    const jobs = await Job.find().populate("createdBy", "-password -__v");
+    res.status(200).json(jobs);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 const createJob = async (req, res) => {
@@ -11,7 +15,54 @@ const createJob = async (req, res) => {
   res.status(200).json(job);
 };
 
+const getJob = async (req, res) => {
+  const { jobId } = req.params;
+  try {
+    const job = await Job.findById(jobId).populate(
+      "createdBy",
+      "-password -__v"
+    );
+    res.status(200).json(job);
+  } catch (error) {
+    res.status(400).json({ message: "Job not found" });
+  }
+};
+
+const updateJob = async (req, res) => {
+  const { jobId } = req.params;
+  try {
+    const job = await Job.findByIdAndUpdate(jobId, req.body, { new: true });
+    res.status(201).json({ message: "Updated" });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const getMyJobs = async (req, res) => {
+  const userId = req.userId;
+  try {
+    const jobs = await Job.find({ createdBy: userId });
+    res.status(200).json(jobs);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const deleteJob = async (req, res) => {
+  const { jobId } = req.params;
+  try {
+    await Job.findByIdAndRemove(jobId);
+    res.status(204).json({ message: "success" });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
 module.exports = {
   getAllJobs,
   createJob,
+  getJob,
+  updateJob,
+  getMyJobs,
+  deleteJob,
 };
--- routes/job.js
+++ routes/job.js
@@ -1,9 +1,22 @@
 const express = require("express");
-const { getAllJobs, createJob } = require("../controllers/job");
+const {
+  getAllJobs,
+  createJob,
+  getJob,
+  updateJob,
+  getMyJobs,
+  deleteJob,
+} = require("../controllers/job");
 const protectAuth = require("../middleware/protectAuth");
 
 const router = express.Router();
 
 router.route("/").get(getAllJobs).post(protectAuth, createJob);
+router.route("/myjobs").get(protectAuth, getMyJobs);
+router
+  .route("/:jobId")
+  .get(getJob)
+  .patch(protectAuth, updateJob)
+  .delete(protectAuth, deleteJob);
 
 module.exports = router;
